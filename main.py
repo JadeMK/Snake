749ca74509c1c29cea19057d84530119d9193b03
Jade Kim <JadeKim.dev@gmail.com>
2022-04-25 13:38:54 +0900

    Screen ui setup

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Testing")
+from turtle import Screen
+
+# Screen
+screen = Screen()
+
+# Screen setup
+screen.setup(width=600, height=600)
+screen.bgcolor("SeaGreen")
+screen.title("Snaky Snaky")
+
+
+screen.exitonclick()
