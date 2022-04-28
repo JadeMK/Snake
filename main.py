77f7e83bdcfdb16d16f15d6ef3f168e7f8d9a7da
Jade Kim <JadeKim.dev@gmail.com>
2022-04-28 14:22:44 +0900

    Screen tracer and update

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from turtle import Screen
 from snake import Snake
+import time
 
 # Screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("SeaGreen")
 screen.title("Snaky Snaky")
+# tracer = 0 meaning off
+screen.tracer(0)
 
 # Snake
 snake = Snake()
@@ -19,6 +22,8 @@ screen.onkey(snake.right, "Right")
 
 # Snake movement test loop
 while True:
+    screen.update()
+    time.sleep(0.1)
     snake.move()
 
 screen.exitonclick()
